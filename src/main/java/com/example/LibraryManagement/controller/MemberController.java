af09919eab5d8a48772992bfa8d7f9ea3c9e19c2
Neeraj Khatri <neerajkhatri@54s-MacBook-Air.local>
2024-01-22 18:31:09 +0530

    Implement controller layer

---
+++ src/main/java/com/example/LibraryManagement/controller/MemberController.java
@@ -0,0 +1,52 @@
+package com.example.LibraryManagement.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.LibraryManagement.model.Book;
+import com.example.LibraryManagement.model.Member;
+import com.example.LibraryManagement.service.inter.MemberService;
+
+@RestController
+@RequestMapping("/api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<String> saveMember(@RequestBody Member member) {
+        try {
+            memberService.saveMember(member);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Member saved successfully");
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error saving member");
+        }
+    }
+
+    @GetMapping("/{memberId}/checked-out-books")
+    public ResponseEntity<List<Book>> getBooksCheckedOutByMembers(@PathVariable int memberId) {
+        try {
+            List<Book> checkedOutBooks = memberService.getBooksCheckedOutByMember(memberId);
+            return ResponseEntity.ok(checkedOutBooks);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+}
---
+++ src/main/java/com/example/LibraryManagement/controller/ReserveController.java
@@ -0,0 +1,50 @@
+package com.example.LibraryManagement.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.LibraryManagement.model.Reserve;
+import com.example.LibraryManagement.service.inter.ReserveService;
+
+@RestController
+@RequestMapping("/api/allocate")
+public class ReserveController {
+
+    private final ReserveService reserveService;
+
+
+    @Autowired
+    public ReserveController(ReserveService reserveService) {
+        this.reserveService = reserveService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<String> saveReserve(@RequestBody Reserve reserve) {
+        try {
+            reserveService.saveReserve(reserve);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Reserve saved successfully");
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error reserving member");
+        }
+    }
+
+    public ResponseEntity<List<Reserve>> getReserveByMemberId(@PathVariable int memberId) {
+        try {
+            List<Reserve> reserveList = reserveService.getReserveByMemberId(memberId);
+            return ResponseEntity.ok(reserveList);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+}
